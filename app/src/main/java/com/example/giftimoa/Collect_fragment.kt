fb699b34d0b6cf1da35a780e30fe676a1b2f2b51
BeomsuMoon <105366422+BeomsuMoon@users.noreply.github.com>
2023-10-23 00:58:27 +0900

    2

--- app/src/main/java/com/example/giftimoa/Collect_fragment.kt
+++ app/src/main/java/com/example/giftimoa/Collect_fragment.kt
@@ -1,5 +1,6 @@
 package com.example.giftimoa
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,13 +10,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Collect_fragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true) // 프래그먼트에서 옵션 메뉴 사용을 활성화
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // 플로팅 버튼 클릭 시 다음 화면의 액티비티로 이동
+        view.findViewById<FloatingActionButton>(R.id.fab_btn).setOnClickListener {
+            val intent = Intent(requireContext(), Collect_gift_activity::class.java)
+            startActivity(intent)
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
---
+++ app/src/main/java/com/example/giftimoa/Collect_gift_activity.kt
@@ -0,0 +1,12 @@
+package com.example.giftimoa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Collect_gift_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collect_gift_add) // 해당 액티비티의 레이아웃 XML 파일을 설정해야 합니다
+    }
+}
