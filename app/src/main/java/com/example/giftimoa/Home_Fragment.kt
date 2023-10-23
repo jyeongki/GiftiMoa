df3f92fbbcf881ea4430cde8b7eac2d61d9b76bc
1817-3 <kho4514@naver.com>
2023-10-23 19:44:32 +0900

    1. New Fragment Update 2. Home_Fragment_List add and TabLayout add

--- app/src/main/java/com/example/giftimoa/Home_Fragment.kt
+++ app/src/main/java/com/example/giftimoa/Home_Fragment.kt
@@ -1,9 +1,12 @@
 package com.example.giftimoa
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -22,6 +25,47 @@ class Home_Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_home_, container, false)
+        val commonClickListener = ClickListeners.getCommonClickListener(requireActivity())
+
+        //커피 아이콘 인텐트
+        val Home_menu_list_coffee = rootView.findViewById<TextView>(R.id.home_ic_coffee)
+        Home_menu_list_coffee.setOnClickListener(commonClickListener)
+
+        //치킨 아이콘 인텐트
+        val Home_menu_list_chiken = rootView.findViewById<TextView>(R.id.home_ic_chicken)
+        Home_menu_list_chiken.setOnClickListener(commonClickListener)
+
+        //피자 아이콘 인텐트
+        val Home_menu_list_pizza = rootView.findViewById<TextView>(R.id.home_ic_pizza)
+        Home_menu_list_pizza.setOnClickListener(commonClickListener)
+
+        //패스트푸드 아이콘 인텐트
+        val Home_menu_list_fastfood = rootView.findViewById<TextView>(R.id.home_ic_festfood)
+        Home_menu_list_fastfood.setOnClickListener(commonClickListener)
+
+        //마트,편의점 아이콘 인텐트
+        val Home_menu_list_mart = rootView.findViewById<TextView>(R.id.home_ic_mart)
+        Home_menu_list_mart.setOnClickListener(commonClickListener)
+
+        //베이커리 아이콘 인텐트
+        val Home_menu_list_bakery = rootView.findViewById<TextView>(R.id.home_ic_bread)
+        Home_menu_list_bakery.setOnClickListener(commonClickListener)
+
+        //아이스크림 아이콘 인텐트
+        val Home_menu_list_icecream = rootView.findViewById<TextView>(R.id.home_ic_icecream)
+        Home_menu_list_icecream.setOnClickListener(commonClickListener)
+
+        //스낵 아이콘 인텐트
+        val Home_menu_list_snack = rootView.findViewById<TextView>(R.id.home_ic_snack)
+        Home_menu_list_snack.setOnClickListener(commonClickListener)
+
+        //영화 아이콘 인텐트
+        val Home_menu_list_movie = rootView.findViewById<TextView>(R.id.home_ic_movie)
+        Home_menu_list_movie.setOnClickListener(commonClickListener)
+
+        //상품권 아이콘 인텐트
+        val Home_menu_list_giftcard = rootView.findViewById<TextView>(R.id.home_ic_giftcard)
+        Home_menu_list_giftcard.setOnClickListener(commonClickListener)
 
         // ViewPager2
         mPager = rootView.findViewById(R.id.viewpager)
@@ -55,7 +99,19 @@ class Home_Fragment : Fragment() {
                 mIndicator.animatePageSelected(position % numPage)
             }
         })
-
         return rootView
+    }
+
+
+    class ClickListeners {
+        companion object {
+            //공용 리스너 클래스 정의
+            fun getCommonClickListener(activity: Activity): View.OnClickListener {
+                return View.OnClickListener {
+                    val intent = Intent(activity, Search_gift_activity::class.java)
+                    activity.startActivity(intent)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/giftimoa/recyclierview_adpater_list/HomeFragment_adpater_list.kt
@@ -0,0 +1,36 @@
+package com.example.giftimoa.recyclierview_adpater_list
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.giftimoa.Home_Fragment_List_Bakery
+import com.example.giftimoa.Home_Fragment_List_Chicken
+import com.example.giftimoa.Home_Fragment_List_Coffee
+import com.example.giftimoa.Home_Fragment_List_Fastfood
+import com.example.giftimoa.Home_Fragment_List_Giftcard
+import com.example.giftimoa.Home_Fragment_List_Icecream
+import com.example.giftimoa.Home_Fragment_List_Mart
+import com.example.giftimoa.Home_Fragment_List_Movie
+import com.example.giftimoa.Home_Fragment_List_Pizza
+import com.example.giftimoa.Home_Fragment_List_Snack
+
+
+class HomeFragment_adpater_list(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = 10 // 탭 개수
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> Home_Fragment_List_Coffee()
+            1 -> Home_Fragment_List_Chicken()
+            2 -> Home_Fragment_List_Pizza()
+            3 -> Home_Fragment_List_Fastfood()
+            4 -> Home_Fragment_List_Mart()
+            5 -> Home_Fragment_List_Bakery()
+            6 -> Home_Fragment_List_Snack()
+            7 -> Home_Fragment_List_Movie()
+            8 -> Home_Fragment_List_Icecream()
+            9 -> Home_Fragment_List_Giftcard()
+            else -> Home_Fragment_List_Coffee() // 기본값 설정
+        }
+    }
+}
