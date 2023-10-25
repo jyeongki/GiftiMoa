fe5912e4982ea435561486825a9713580a92bfd3
1817-3 <kho4514@naver.com>
2023-10-25 20:01:18 +0900

    1. New Fragment Update 2. Home_Fragment_List add and TabLayout add

--- app/src/main/java/com/example/giftimoa/Collect_gift_add_activity.kt
+++ app/src/main/java/com/example/giftimoa/Collect_gift_add_activity.kt
@@ -1,8 +1,6 @@
 package com.example.giftimoa
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class Collect_gift_add_activity : AppCompatActivity() {
@@ -13,17 +11,4 @@ class Collect_gift_add_activity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            android.R.id.home -> {
-                val intent = Intent().apply {
-                    //원하는 코드 입력
-                }
-                setResult()//원하는 코드 입력
-                finish()
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
