437c7c82cd53d84d9541bca51f6cd9175af47a96
BeomsuMoon <105366422+BeomsuMoon@users.noreply.github.com>
2023-11-06 07:54:34 +0900

    update 20231106

---
+++ app/src/main/java/com/example/giftimoa/Collect_gift_add_info_activity.kt
@@ -0,0 +1,28 @@
+package com.example.giftimoa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.giftimoa.databinding.LayoutCollectGiftAddBinding
+import com.example.giftimoa.databinding.LayoutCollectGiftAddInfoBinding
+
+class Collect_gift_add_info_activity : AppCompatActivity() {
+    private lateinit var binding : LayoutCollectGiftAddInfoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LayoutCollectGiftAddInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //액션바 활성화
+        setSupportActionBar(binding.myToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "기프티콘 정보"
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean { // 액션바 뒤로가기
+        onBackPressed()
+        return true
+    }
+
+}
