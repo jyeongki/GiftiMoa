8d095e423dbb588054ee77258cf32d28759e4afb
1817-3 <kho4514@naver.com>
2023-11-08 23:59:49 +0900

    등록 아이템 해당 데이터 갖고 인텐트 액티비티

--- app/src/main/java/com/example/giftimoa/Collect_gift_add_info_activity.kt
+++ app/src/main/java/com/example/giftimoa/Collect_gift_add_info_activity.kt
@@ -1,9 +1,11 @@
 package com.example.giftimoa
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.giftimoa.databinding.LayoutCollectGiftAddBinding
 import com.example.giftimoa.databinding.LayoutCollectGiftAddInfoBinding
+import com.example.giftimoa.dto.Collect_Gift
 
 class Collect_gift_add_info_activity : AppCompatActivity() {
     private lateinit var binding : LayoutCollectGiftAddInfoBinding
@@ -18,12 +20,20 @@ class Collect_gift_add_info_activity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "기프티콘 정보"
 
+        val gift = intent.getSerializableExtra("gift") as Collect_Gift
+        var giftNameTextView = binding.textGiftName
+        var textEffectiveDateTextView = binding.textEffectiveDate
+        var textBarcodeTextView = binding.textBarcode
+        var textUsageTextView = binding.textUsage
+
+        giftNameTextView.text = gift.giftName
+        textEffectiveDateTextView.text = gift.effectiveDate
+        textBarcodeTextView.text = gift.barcode
+        textUsageTextView.text = gift.usage
+
     }
 
-    var gift_Name = intent.getStringExtra("gift_Name")
-    var gift_effectiveDate = intent.getStringExtra("gift_effectiveDate")
-    var gift_barcode = intent.getStringExtra("gift_barcode")
-    var gift_usage = intent.getStringExtra("gift_usage")
+
     override fun onSupportNavigateUp(): Boolean { // 액션바 뒤로가기
         onBackPressed()
         return true
