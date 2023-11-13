69538d9d4dca9ff32935289f1b6f0765caf72566
1817-3 <kho4514@naver.com>
2023-11-13 13:39:15 +0900

    아이디 찾기 비밀번호 찾기 기능 추가

--- app/src/main/java/com/example/giftimoa/PW_Find_Result_activity.kt
+++ app/src/main/java/com/example/giftimoa/PW_Find_Result_activity.kt
@@ -2,11 +2,25 @@ package com.example.giftimoa
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log  // 추가: Log 사용을 위한 import 문
 import androidx.appcompat.app.AppCompatActivity
 import com.example.giftimoa.databinding.LayoutSearchPwResultBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import android.widget.Toast
 
 class PW_Find_Result_activity : AppCompatActivity() {
-    private lateinit var binding : LayoutSearchPwResultBinding
+    private lateinit var binding: LayoutSearchPwResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LayoutSearchPwResultBinding.inflate(layoutInflater)
@@ -16,18 +30,121 @@ class PW_Find_Result_activity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        nextpw_btn()
-        cancle_btn()
+        // Intent에서 이메일 값을 가져옴
+        val email = intent.getStringExtra("email_text")
+        // 이메일 값을 이메일 입력 칸에 설정
+        binding.emailEditText.setText(email)
+        // EditText를 비활성화
+        binding.emailEditText.isEnabled = false
+
+        nextPwBtn()
+        cancelBtn()
     }
-    private fun nextpw_btn(){
+
+    private fun passwordCheck(password: String): Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#\$%^&+=]).{8,}$"
+        return password.matches(passwordPattern.toRegex())
+    }
+
+    private fun nextPwBtn() {
         binding.nextViewBtn.setOnClickListener {
-            val intent = Intent(this@PW_Find_Result_activity, Login_activity::class.java)
-            startActivity(intent)
-            finish()
+            // 사용자 입력에서 새 비밀번호 가져오기
+            // 사용자 입력에서 새 비밀번호 가져오기
+            val newPassword = binding.passwordEditText.text.toString()
+            val confirmPassword = binding.certificationNum.text.toString()
+
+            // 비밀번호 유효성 체크
+            if (passwordCheck(newPassword)) {
+                // 비밀번호가 일치하는지 확인
+                if (newPassword == confirmPassword) {
+                    // 서버의 데이터베이스에서 비밀번호 업데이트
+                    updatePasswordOnServer(newPassword)
+                } else {
+                    Toast.makeText(this@PW_Find_Result_activity, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@PW_Find_Result_activity, "비밀번호는 8자 이상이어야 하며, 영어, 숫자, 특수 문자를 포함해야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
-    private fun cancle_btn(){
+
+    private fun updatePasswordOnServer(newPassword: String) {
+        val email = intent.getStringExtra("email_text")
+
+        // 서버에 비밀번호를 업데이트하도록 요청
+        val url = "http://3.35.110.246:3306/updatePassword"
+        val json = JsonObject().apply {
+            addProperty("email", email)
+            addProperty("newPassword", newPassword)
+        }
+
+        val mediaType = "application/json".toMediaType()
+        val requestBody = json.toString().toRequestBody(mediaType)
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response: Response = withContext(Dispatchers.IO) {
+                    OkHttpClient().newCall(request).execute()
+                }
+
+                runOnUiThread {
+                    if (response.isSuccessful) {
+                        val responseData = response.body?.string()
+                        val jsonObject = JsonParser().parse(responseData).asJsonObject
+
+                        // 서버 응답을 필요에 따라 처리
+                        handleServerResponse(jsonObject)
+                    } else {
+                        // 실패한 응답 처리...
+                        Toast.makeText(
+                            this@PW_Find_Result_activity,
+                            "비밀번호 업데이트에 실패했습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                runOnUiThread {
+                    Log.e("PW_Find_Result_activity", "에러: ${e.message}")  // 추가: 예외 로그 출력
+                    Toast.makeText(
+                        this@PW_Find_Result_activity,
+                        "오류 발생: ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun handleServerResponse(jsonObject: JsonObject) {
+        // 서버 응답을 필요에 따라 처리
+        val message = jsonObject.get("message").asString
+        if (message == "비밀번호가 성공적으로 업데이트되었습니다.") {
+            Toast.makeText(
+                this@PW_Find_Result_activity,
+                "비밀번호가 성공적으로 업데이트되었습니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+            val intent = Intent(this@PW_Find_Result_activity, Login_activity::class.java)
+            startActivity(intent)
+            finish() // 현재 액티비티 종료
+
+        } else {
+            val errorMessage = jsonObject.get("message").asString
+            Toast.makeText(this@PW_Find_Result_activity, errorMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun cancelBtn() {
         binding.cancelButton.setOnClickListener {
+            // 로그인 화면으로 이동
             val intent = Intent(this@PW_Find_Result_activity, Login_activity::class.java)
             startActivity(intent)
             finish()
--- app/src/main/java/com/example/giftimoa/PW_Find_activity.kt
+++ app/src/main/java/com/example/giftimoa/PW_Find_activity.kt
@@ -2,36 +2,146 @@ package com.example.giftimoa
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.giftimoa.databinding.LayoutCollectGiftAddBinding
-import com.example.giftimoa.databinding.LayoutSearchIdBinding
-import com.example.giftimoa.databinding.LayoutSearchPwBinding
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
 
 class PW_Find_activity : AppCompatActivity() {
-    private lateinit var binding : LayoutSearchPwBinding
+    private lateinit var emailEditText: EditText
+    private lateinit var certificationNum: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LayoutSearchPwBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.layout_search_pw)
 
-        setSupportActionBar(binding.myToolbar)
+        emailEditText = findViewById(R.id.password_editText)
+        certificationNum = findViewById(R.id.certification_num)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        nextpw_btn()
+        findViewById<Button>(R.id.email_double_check).setOnClickListener {
+            val email = emailEditText.text.toString()
+
+            if (email.isNotEmpty()) {
+                // 이메일 확인 요청
+                checkEmailExist(email)
+            } else {
+                Toast.makeText(this, "이메일 주소를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        findViewById<Button>(R.id.nextView_btn).setOnClickListener {
+            val enteredVerificationCode = certificationNum.text.toString()
+
+            if (enteredVerificationCode.isNotEmpty()) {
+                // 인증 코드 확인 요청
+                verifyVerificationCode(emailEditText.text.toString(), enteredVerificationCode)
+            } else {
+                Toast.makeText(this, "인증 코드를 입력해주세요..", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
-    private fun nextpw_btn(){
-        binding.nextViewBtn.setOnClickListener {
-            val intent = Intent(this@PW_Find_activity, PW_Find_Result_activity::class.java)
-            startActivity(intent)
-            finish()
-        }
+    // 이메일 존재 여부 확인을 수행하는 메서드
+    private fun checkEmailExist(email: String) {
+        val client = OkHttpClient()
+        val url = "http://3.35.110.246:3306/checkEmail" // 변경된 서버 엔드포인트
+        val json = """{"email": "$email"}"""
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = json.toRequestBody(mediaType)
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this@PW_Find_activity, "이메일 확인 요청 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseText = response.body?.string()
+                val jsonResponse = JSONObject(responseText)
+
+                if (response.isSuccessful) {
+                    val message = jsonResponse.getString("message")
+                    runOnUiThread {
+                        Toast.makeText(this@PW_Find_activity, message, Toast.LENGTH_SHORT).show()
+
+                        if (message == "이메일이 확인되었고, 인증 코드를 이메일로 전송했습니다.") {
+                            // 확인이 성공한 경우, 이메일 입력 필드를 비활성화
+                            emailEditText.isEnabled = false
+                            emailEditText.isFocusable = false
+                        }
+                    }
+                } else {
+                    val error = jsonResponse.getString("error")
+                    runOnUiThread {
+                        Toast.makeText(this@PW_Find_activity, error, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+    }
+
+    // 인증 코드 확인을 수행하는 메서드
+    private fun verifyVerificationCode(email: String, enteredCode: String) {
+        val client = OkHttpClient()
+        val url = "http://3.35.110.246:3306/SignUpEmail_node" // 서버의 인증 코드 확인 엔드포인트
+        val json = """{"email": "$email", "verCode": "$enteredCode"}"""
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = json.toRequestBody(mediaType)
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this@PW_Find_activity, "인증 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseText = response.body?.string()
+                val jsonResponse = JSONObject(responseText)
+
+                if (response.isSuccessful) {
+                    val message = jsonResponse.getString("message")
+                    runOnUiThread {
+                        Toast.makeText(this@PW_Find_activity, message, Toast.LENGTH_SHORT).show()
+
+                        if (message == "Verification successful") {
+                            // 인증 성공 시, 다음 단계로 이동 (예: PasswordResetActivity)
+                            val intent = Intent(this@PW_Find_activity, PW_Find_Result_activity::class.java)
+                            intent.putExtra("email_text", email)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                } else {
+                    val error = jsonResponse.getString("error")
+                    runOnUiThread {
+                        Toast.makeText(this@PW_Find_activity, error, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
-        finish()
         return true
     }
 }
